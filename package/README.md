3f7b69fb230e320e78e204a4a6dda4ced616d59f
cmatthewbrooks <me@cmatthewbrooks.com>
2019-03-23 07:04:25 +0000

    Initial commit

---
+++ package/README.md
@@ -0,0 +1,14 @@
+# Package
+
+A skeleton outline for starting a new Python package
+
+# Installation
+TODO
+# Usage
+TODO
+# Contributing
+TODO
+# Credits
+TODO
+# License
+TODO
---
+++ package/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+
+    name = 'app',
+    version = '0.1',
+    author = 'cmatthewbrooks',
+    author_email = 'me@cmatthewbrooks.com',
+    keywords = [''],
+    packages = [''],
+    entry_points = {
+        'console_scripts': [
+        'app = app.app:main'
+        ]
+    },
+    install_requires = [
+        ''
+    ]
+
+)
